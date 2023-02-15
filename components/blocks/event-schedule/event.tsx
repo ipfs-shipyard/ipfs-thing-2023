f46d950c26300f1a22352dbbbe943ea207050d5d
Tom Benbow <tom@hellomoon.net>
2023-02-15 11:02:02 -0600

    Update submit an event link

--- components/blocks/event-schedule/event.tsx
+++ components/blocks/event-schedule/event.tsx
@@ -108,7 +108,8 @@ function EventModalContent({ event }) {
 
 export function AddCard({ config }) {
   return (
-    <Modal content={<AddEventModalContent config={config} />} title="Submit a track or talk" link="" hash="#add-event">
+    // <Modal content={<AddEventModalContent config={config} />} title="Submit a track or talk" link="" hash="#add-event">
+    <a target="_blank" href="https://airtable.com/shr2Eq1juLJh0ri3i">
       <Card color="white">
         <div className="place-content-center w-full m-0 py-5 text-center text-gray-300 hover:text-gray-500">
           <div className="text-6xl">
@@ -119,7 +120,8 @@ export function AddCard({ config }) {
           </div>
         </div>
       </Card>
-    </Modal>
+    </a>
+    // </Modal>
   )
 }
 
